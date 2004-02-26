46d8110792f256330ca4e590224e2e005b5e7ee4
Egbert Eich <eich@suse.de>
2004-02-26 09:23:18 +0000

    Importing vendor version xf86-4_3_99_903 on Wed Feb 26 01:21:00 PST 2004

--- src/cyrix_shadow.c
+++ src/cyrix_shadow.c
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/cyrix/cyrix_shadow.c,v 1.1 2002/11/06 11:38:59 alanh Exp $ */
+/* $XFree86$ */
 
 /*
    Copyright (c) 1999,  The XFree86 Project Inc. 
